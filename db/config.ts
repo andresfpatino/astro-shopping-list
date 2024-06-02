51b7fcd710955f6022257a657d334c89d940665d
Andrés Patiño <andres@frontporchsolutions.com>
2024-06-02 00:33:31 -0500

    feat: :safety_vest: GitHub OAuth

--- db/config.ts
+++ db/config.ts
@@ -38,6 +38,7 @@ export const User = defineTable({
 		id: column.text({ primaryKey: true, optional: false, unique: true }),
 		username: column.text({ unique: true, optional: false }),
 		password: column.text( { optional: true } ),
+		github_id: column.text({ optional: true, unique: true }),
 	},
 });
 
